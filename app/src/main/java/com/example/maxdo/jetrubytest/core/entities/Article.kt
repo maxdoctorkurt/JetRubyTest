b1ee88cc5dea182560bd53463022cfd84db807e4
maxdoctorkurt@mail.ru <querty123>
2019-01-27 05:50:24 +0300

    room dao fix

--- app/src/main/java/com/example/maxdo/jetrubytest/core/entities/Article.kt
+++ app/src/main/java/com/example/maxdo/jetrubytest/core/entities/Article.kt
@@ -1,10 +1,15 @@
 package com.example.maxdo.jetrubytest.core.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 class Article(
     val source: ArticleSource?,
     val author: String?,
     val title: String?,
     val description: String?,
+    @PrimaryKey(autoGenerate = false)
     val url: String?,
     val urlToImage: String?,
     // The date and time that the article was published, in UTC (+000)
